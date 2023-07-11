70448bae599657af9b6a60b0e2c02435a52b9156
sudodebug <sudodebug@gmail.com>
2023-07-11 12:17:17 +0500

    Create content models(text, image, file, video)

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 from courses.fields import OrderField
 
 
@@ -43,3 +44,44 @@ class Module(models.Model):
 
     def __str__(self):
         return f'{self.order}. {self.title}'
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,
+                                     on_delete=models.CASCADE,
+                                     limit_choices_to={'model__in': ('text', 'file', 'image', 'video')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    file = models.ImageField(upload_to='images')
+
+
+class Video(ItemBase):
+    url = models.URLField()
+
+
+
