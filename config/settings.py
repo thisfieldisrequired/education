a728a43758333e6dd1bfc9b4070355319c8aeb1a
sudodebug <sudodebug@gmail.com>
2023-07-13 16:38:40 +0500

    Fix logout.html display template

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'courses.apps.CoursesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'courses.apps.CoursesConfig',
+
 ]
 
 MIDDLEWARE = [
