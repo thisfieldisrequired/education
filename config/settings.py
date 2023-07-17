f6c21a107e05009c2893e14ff1dc8aa195cac20d
sudodebug <sudodebug@gmail.com>
2023-07-17 20:09:34 +0500

    Create students apps, students registration

--- config/settings.py
+++ config/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'courses.apps.CoursesConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'students.apps.StudentsConfig',
 
 
 ]
