344e8dfadf7f8620eb583aecacb770b92cb2dde6
sudodebug <sudodebug@gmail.com>
2023-07-17 18:44:04 +0500

    Create CourseListView, CourseDetailView

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from courses.views import CourseListView
 
 
 urlpatterns = [
@@ -11,6 +12,8 @@ urlpatterns = [
     path('course/', include('courses.urls')),
 
     path('admin/', admin.site.urls),
+
+    path('', CourseListView.as_view(), name='course_list'),
 ]
 
 if settings.DEBUG:
--- courses/views.py
+++ courses/views.py
@@ -9,6 +9,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMix
 from django.urls import reverse_lazy
 from .models import Course, Module, Content
 from braces.views import JSONRequestResponseMixin, CsrfExemptMixin
+from django.db.models import Count
+from .models import Subject
+from django.views.generic.detail import DetailView
 
 
 class OwnerMixin:
@@ -150,11 +153,24 @@ class ContentOrderView(CsrfExemptMixin, JSONRequestResponseMixin, View):
         return self.render_json_response({'saved': 'OK'})
 
 
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject,
+                                        'courses': courses})
 
 
-
-
-
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
 
 
 
