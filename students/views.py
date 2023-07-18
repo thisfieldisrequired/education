d7620113d7701942c87d43ff35510f91e42b605c
sudodebug <sudodebug@gmail.com>
2023-07-18 17:09:05 +0500

    Added the ability to enroll in a course. Displaying the List of courses. Detailed information about the course.

--- students/views.py
+++ students/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, ListView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.views.generic.edit import FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import CourseEnrollForm
+from courses.models import Course
 
 
 class StudentRegistrationView(CreateView):
@@ -15,7 +16,7 @@ class StudentRegistrationView(CreateView):
 
     def form_valid(self, form):
         result = super().form_valid(form)
-        cd = form.changed_data
+        cd = form.cleaned_data
         user = authenticate(username=cd['username'], password=cd['password1'])
         login(self.request, user)
         return result
@@ -26,9 +27,36 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
     form_class = CourseEnrollForm
 
     def form_valid(self, form):
-        self.course = form.changed_data['course']
+        self.course = form.cleaned_data['course']
         self.course.students.add(self.request.user)
         return super().form_valid(form)
 
     def get_success_url(self):
         return reverse_lazy('student_course_detail', args=[self.course.id])
+
+
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+
+class StudentCourseDetailView(DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            context['module'] = course.modules.get(id=self.kwargs['module_id'])
+        else:
+            context['module'] = course.modules.all()[0]
+        return context
