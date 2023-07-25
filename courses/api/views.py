c729311c9ede0b2b78626c3f471c40ce3fe04434
sudodebug <sudodebug@gmail.com>
2023-07-25 16:41:09 +0500

    Create CourseViewSet

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,9 +1,12 @@
 from rest_framework import generics
 from courses.models import Subject, Course
-from courses.api.serializers import SubjectSerializer
+from courses.api.serializers import SubjectSerializer, CourseSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 
 
 class SubjectListView(generics.ListAPIView):
@@ -17,7 +20,15 @@ class SubjectDetailView(generics.RetrieveAPIView):
 
 
 class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
