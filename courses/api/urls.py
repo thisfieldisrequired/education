59e4a4e5103129270efc90b937476bca0a78d7d6
sudodebug <sudodebug@gmail.com>
2023-07-25 15:32:50 +0500

    Create Course enroll API

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -8,4 +8,5 @@ app_name = 'courses'
 urlpatterns = [
     path('subjects/', views.SubjectListView.as_view(), name='subject_list'),
     path('subjects/<int:pk>/', views.SubjectDetailView.as_view(), name='subject_detail'),
+    path('courses/<int:pk>/enroll/', views.CourseEnrollView.as_view(), name='course_enroll'),
 ]
