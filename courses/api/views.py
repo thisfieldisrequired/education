91fc4ad17c90c8145b610e31604bb7caf9392f5f
sudodebug <sudodebug@gmail.com>
2023-07-26 16:55:52 +0500

    Implemented adding students to the course in CourseViewSet

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from courses.models import Subject, Course
-from courses.api.serializers import SubjectSerializer, CourseSerializer
+from courses.api.serializers import SubjectSerializer, CourseSerializer, CourseWithContentSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,6 +8,7 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from courses.api.permissions import IsEnrolled
 
 
 class SubjectListView(generics.ListAPIView):
@@ -42,3 +43,11 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @action(detail=True,
+            methods=['get'],
+            serializer_class=CourseWithContentSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
